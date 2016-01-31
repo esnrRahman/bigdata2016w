25ded817fee17009a24f2710219ed0d6686f55e1
Ehsanur Rahman <esnr.rahman@gmail.com>
2016-01-31 16:48:15 -0500

    Override stuff to make PairOfWritables work

--- src/main/java/ca/uwaterloo/cs/bigdata2016w/esnrRahman/assignment3/BuildInvertedIndexCompressed.java
+++ src/main/java/ca/uwaterloo/cs/bigdata2016w/esnrRahman/assignment3/BuildInvertedIndexCompressed.java
@@ -14,9 +14,12 @@ import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
 import org.apache.hadoop.io.WritableUtils;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -35,163 +38,198 @@ import tl.lin.data.pair.PairOfObjectInt;
 import tl.lin.data.pair.PairOfWritables;
 
 public class BuildInvertedIndexCompressed extends Configured implements Tool {
-  private static final Logger LOG = Logger.getLogger(BuildInvertedIndexCompressed.class);
+    private static final Logger LOG = Logger.getLogger(BuildInvertedIndexCompressed.class);
 
-  private static class MyMapper extends Mapper<LongWritable, Text, PairOfWritables<Text, IntWritable>, IntWritable> {
-    private static final Text WORD = new Text();
-    private static final Object2IntFrequencyDistribution<String> COUNTS =
-        new Object2IntFrequencyDistributionEntry<String>();
-    private static final PairOfWritables<Text, IntWritable> KEYPAIR = new PairOfWritables<Text, IntWritable>();
-    private static final IntWritable DOCID = new IntWritable();
-    private static final IntWritable TF = new IntWritable();
-    @Override
-    public void map(LongWritable docno, Text doc, Context context)
-        throws IOException, InterruptedException {
-      String text = doc.toString();
+    private static class MyMapper extends Mapper<LongWritable, Text, PairOfWritables<Text, IntWritable>, IntWritable> {
+        private static final Text WORD = new Text();
+        private static final Object2IntFrequencyDistribution<String> COUNTS =
+                new Object2IntFrequencyDistributionEntry<String>();
+        private static final PairOfWritables<Text, IntWritable> KEYPAIR = new PairOfWritables<Text, IntWritable>();
+        private static final IntWritable DOCID = new IntWritable();
+        private static final IntWritable TF = new IntWritable();
 
-      // Tokenize line.
-      List<String> tokens = new ArrayList<String>();
-      StringTokenizer itr = new StringTokenizer(text);
-      while (itr.hasMoreTokens()) {
-        String w = itr.nextToken().toLowerCase().replaceAll("(^[^a-z]+|[^a-z]+$)", "");
-        if (w.length() == 0) continue;
-        tokens.add(w);
-      }
+        @Override
+        public void map(LongWritable docno, Text doc, Context context)
+                throws IOException, InterruptedException {
+            String text = doc.toString();
 
-      // Build a histogram of the terms.
-      COUNTS.clear();
-      for (String token : tokens) {
-        COUNTS.increment(token);
-      }
+            // Tokenize line.
+            List<String> tokens = new ArrayList<String>();
+            StringTokenizer itr = new StringTokenizer(text);
+            while (itr.hasMoreTokens()) {
+                String w = itr.nextToken().toLowerCase().replaceAll("(^[^a-z]+|[^a-z]+$)", "");
+                if (w.length() == 0) continue;
+                tokens.add(w);
+            }
 
-      // Emit postings in the form of ((term, df), tf)
-      for (PairOfObjectInt<String> e : COUNTS) {
-        WORD.set(e.getLeftElement());
-        DOCID.set((int) docno.get());
-        TF.set(e.getRightElement());
-        KEYPAIR.set(WORD, DOCID);
-        context.write(KEYPAIR, TF);
-      }
-    }
-  }
+            // Build a histogram of the terms.
+            COUNTS.clear();
+            for (String token : tokens) {
+                COUNTS.increment(token);
+            }
 
-//  protected static class MyPartitioner extends Partitioner<PairOfStrings, FloatWritable> {
-//    @Override
-//    public int getPartition(PairOfStrings key, FloatWritable value, int numReduceTasks) {
-//      return (key.getLeftElement().hashCode() & Integer.MAX_VALUE) % numReduceTasks;
-//    }
-//  }
-
-  private static class MyReducer extends
-      Reducer<PairOfWritables<Text, IntWritable>, IntWritable, Text, PairOfWritables<IntWritable, BytesWritable>> {
-    private final static IntWritable DF = new IntWritable();
-    private static final Text PREVTERM = new Text();
-    private static final Text CURRTERM = new Text();
-    private static int dGap = 0;
-    private static int lastDocId = 0;
-    private static boolean FLAG = true;
-    private static final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-    private static final DataOutputStream outputStream = new DataOutputStream(byteArrayOutputStream);
-
-    @Override
-    public void reduce(PairOfWritables<Text, IntWritable> keyPair, Iterable<IntWritable> values, Context context)
-        throws IOException, InterruptedException {
-
-      int documentFrequency = 0;
-
-      if (FLAG) {
-        PREVTERM.set(keyPair.getLeftElement());
-        FLAG = false;
-      }
-
-      CURRTERM.set(keyPair.getLeftElement());
-
-      if (!CURRTERM.equals(PREVTERM)) {
-        DF.set(documentFrequency);
-        BytesWritable bytesWritable = new BytesWritable(byteArrayOutputStream.toByteArray());
-        context.write(CURRTERM, new PairOfWritables<IntWritable, BytesWritable>(DF, bytesWritable));
-
-        dGap = 0;
-        lastDocId = 0;
-        documentFrequency = 0;
-        byteArrayOutputStream.reset();
-      }
-
-      // Append in the BytesWritable. Equivalent to P.APPEND(<n ,f>)
-      for (IntWritable tf : values) {
-        documentFrequency += 1;
-        dGap = keyPair.getRightElement().get() - lastDocId;
-        lastDocId = keyPair.getRightElement().get();
-        WritableUtils.writeVInt(outputStream, dGap);
-        WritableUtils.writeVInt(outputStream, tf.get());
-      }
-      PREVTERM.set(CURRTERM.toString());
-
-    }
-  }
-
-  private BuildInvertedIndexCompressed() {}
-
-  public static class Args {
-    @Option(name = "-input", metaVar = "[path]", required = true, usage = "input path")
-    public String input;
-
-    @Option(name = "-output", metaVar = "[path]", required = true, usage = "output path")
-    public String output;
-  }
-
-  /**
-   * Runs this tool.
-   */
-  public int run(String[] argv) throws Exception {
-    Args args = new Args();
-    CmdLineParser parser = new CmdLineParser(args, ParserProperties.defaults().withUsageWidth(100));
-
-    try {
-      parser.parseArgument(argv);
-    } catch (CmdLineException e) {
-      System.err.println(e.getMessage());
-      parser.printUsage(System.err);
-      return -1;
+            // Emit postings in the form of ((term, df), tf)
+            for (PairOfObjectInt<String> e : COUNTS) {
+                WORD.set(e.getLeftElement());
+                DOCID.set((int) docno.get());
+                TF.set(e.getRightElement());
+                KEYPAIR.set(WORD, DOCID);
+                context.write(KEYPAIR, TF);
+            }
+        }
     }
 
-    LOG.info("Tool: " + BuildInvertedIndexCompressed.class.getSimpleName());
-    LOG.info(" - input path: " + args.input);
-    LOG.info(" - output path: " + args.output);
+    private static class MyKeyComparator extends WritableComparator {
+        protected MyKeyComparator() {
+            super(PairOfWritableComparables.class, true);
+        }
 
-    Job job = Job.getInstance(getConf());
-    job.setJobName(BuildInvertedIndexCompressed.class.getSimpleName());
-    job.setJarByClass(BuildInvertedIndexCompressed.class);
+        @Override
+        public int compare(WritableComparable wc1, WritableComparable wc2) {
+            PairOfWritableComparables pair1 = (PairOfWritableComparables) wc1;
+            PairOfWritableComparables pair2 = (PairOfWritableComparables) wc2;
 
-    job.setNumReduceTasks(1);
+            return pair1.compareTo(pair2);
+        }
+    }
 
-    FileInputFormat.setInputPaths(job, new Path(args.input));
-    FileOutputFormat.setOutputPath(job, new Path(args.output));
+    private static class PairOfWritableComparables extends
+            PairOfWritables<Text, IntWritable>
+            implements WritableComparable<PairOfWritables<Text, IntWritable>> {
 
-    job.setMapOutputKeyClass(PairOfWritables.class);
-    job.setMapOutputValueClass(IntWritable.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(PairOfWritables.class);
-    job.setOutputFormatClass(MapFileOutputFormat.class);
+        @Override
+        public int compareTo(PairOfWritables<Text, IntWritable> pow1) {
 
-    job.setMapperClass(MyMapper.class);
-    job.setReducerClass(MyReducer.class);
+            // Compare each element of the pair with the other pair
+            int compareValue = this.getLeftElement().toString().compareTo(pow1.getLeftElement().toString());
+            if (compareValue == 0) {
+                compareValue = this.getRightElement().compareTo(pow1.getRightElement());
+            }
+            return compareValue;
+        }
+    }
 
-    // Delete the output directory if it exists already.
-    Path outputDir = new Path(args.output);
-    FileSystem.get(getConf()).delete(outputDir, true);
 
-    long startTime = System.currentTimeMillis();
-    job.waitForCompletion(true);
-    System.out.println("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
+    private static class MyPartitioner extends Partitioner<PairOfWritableComparables, IntWritable> {
+        @Override
+        public int getPartition(PairOfWritableComparables key, IntWritable value, int numReduceTasks) {
+            return key.getLeftElement().hashCode() % numReduceTasks;
+        }
+    }
 
-    return 0;
-  }
+    private static class MyReducer extends
+            Reducer<PairOfWritables<Text, IntWritable>, IntWritable, Text, PairOfWritables<IntWritable, BytesWritable>> {
+        private final static IntWritable DF = new IntWritable();
+        private static final Text PREVTERM = new Text();
+        private static final Text CURRTERM = new Text();
+        private static int dGap = 0;
+        private static int lastDocId = 0;
+        private static boolean FLAG = true;
+        private static final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        private static final DataOutputStream outputStream = new DataOutputStream(byteArrayOutputStream);
 
-  /**
-   * Dispatches command-line arguments to the tool via the {@code ToolRunner}.
-   */
-  public static void main(String[] args) throws Exception {
-    ToolRunner.run(new BuildInvertedIndexCompressed(), args);
-  }
+        @Override
+        public void reduce(PairOfWritables<Text, IntWritable> keyPair, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+
+            int documentFrequency = 0;
+
+            if (FLAG) {
+                PREVTERM.set(keyPair.getLeftElement());
+                FLAG = false;
+            }
+
+            CURRTERM.set(keyPair.getLeftElement());
+
+            if (!CURRTERM.equals(PREVTERM)) {
+                DF.set(documentFrequency);
+                BytesWritable bytesWritable = new BytesWritable(byteArrayOutputStream.toByteArray());
+                context.write(CURRTERM, new PairOfWritables<IntWritable, BytesWritable>(DF, bytesWritable));
+
+                dGap = 0;
+                lastDocId = 0;
+                documentFrequency = 0;
+                byteArrayOutputStream.reset();
+            }
+
+            // Append in the BytesWritable. Equivalent to P.APPEND(<n ,f>)
+            for (IntWritable tf : values) {
+                documentFrequency += 1;
+                dGap = keyPair.getRightElement().get() - lastDocId;
+                lastDocId = keyPair.getRightElement().get();
+                WritableUtils.writeVInt(outputStream, dGap);
+                WritableUtils.writeVInt(outputStream, tf.get());
+            }
+            PREVTERM.set(CURRTERM.toString());
+
+        }
+    }
+
+    private BuildInvertedIndexCompressed() {
+    }
+
+    public static class Args {
+        @Option(name = "-input", metaVar = "[path]", required = true, usage = "input path")
+        public String input;
+
+        @Option(name = "-output", metaVar = "[path]", required = true, usage = "output path")
+        public String output;
+    }
+
+    /**
+     * Runs this tool.
+     */
+    public int run(String[] argv) throws Exception {
+        Args args = new Args();
+        CmdLineParser parser = new CmdLineParser(args, ParserProperties.defaults().withUsageWidth(100));
+
+        try {
+            parser.parseArgument(argv);
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            parser.printUsage(System.err);
+            return -1;
+        }
+
+        LOG.info("Tool: " + BuildInvertedIndexCompressed.class.getSimpleName());
+        LOG.info(" - input path: " + args.input);
+        LOG.info(" - output path: " + args.output);
+
+        Job job = Job.getInstance(getConf());
+        job.setJobName(BuildInvertedIndexCompressed.class.getSimpleName());
+        job.setJarByClass(BuildInvertedIndexCompressed.class);
+
+        job.setNumReduceTasks(1);
+
+        FileInputFormat.setInputPaths(job, new Path(args.input));
+        FileOutputFormat.setOutputPath(job, new Path(args.output));
+
+        job.setMapOutputKeyClass(PairOfWritables.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(PairOfWritables.class);
+        job.setOutputFormatClass(MapFileOutputFormat.class);
+
+        job.setMapperClass(MyMapper.class);
+        job.setReducerClass(MyReducer.class);
+        job.setPartitionerClass(MyPartitioner.class);
+        job.setSortComparatorClass(MyKeyComparator.class);
+
+        // Delete the output directory if it exists already.
+        Path outputDir = new Path(args.output);
+        FileSystem.get(getConf()).delete(outputDir, true);
+
+        long startTime = System.currentTimeMillis();
+        job.waitForCompletion(true);
+        System.out.println("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
+
+        return 0;
+    }
+
+    /**
+     * Dispatches command-line arguments to the tool via the {@code ToolRunner}.
+     */
+    public static void main(String[] args) throws Exception {
+        ToolRunner.run(new BuildInvertedIndexCompressed(), args);
+    }
 }
