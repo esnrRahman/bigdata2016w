6aaa779bb8b29c4aa7a5e6c37a87262beb7c130c
Ehsanur Rahman <esnr.rahman@gmail.com>
2016-02-26 13:06:43 -0500

    WIP

--- src/main/java/ca/uwaterloo/cs/bigdata2016w/esnrRahman/assignment5/Q1.scala
+++ src/main/java/ca/uwaterloo/cs/bigdata2016w/esnrRahman/assignment5/Q1.scala
@@ -1,16 +1,15 @@
 package ca.uwaterloo.cs.bigdata2016w.esnrRahman.assignment5
 
 import org.apache.log4j._
-import org.apache.hadoop.fs._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.rogach.scallop._
 
 class Conf(args: Seq[String]) extends ScallopConf(args) {
-  mainOptions = Seq(input, dateparam)
+  mainOptions = Seq(input, date)
   val input = opt[String](descr = "input dir", required = true)
-  val dateparam = opt[String](descr = "date param", required = true)
-  }
+  val date = opt[String](descr = "date param", required = true)
+}
 
 object Q1 {
   val log = Logger.getLogger(getClass().getName())
@@ -19,21 +18,26 @@ object Q1 {
     val args = new Conf(argv)
 
     log.info("Input: " + args.input())
-    log.info("Ship Date: " + args.dateparam())
+    log.info("Ship Date: " + args.date())
 
     val conf = new SparkConf().setAppName("SQL Query 1")
     val sc = new SparkContext(conf)
-    val queriedShipDate = args.dateparam()
+    val queriedShipDate = args.date()
 
     val textFile = sc.textFile(args.input() + "/lineitem.tbl")
 
-    textFile
+    val numberOfItems = textFile
       .flatMap(line => {
-        val shipDate = line.split("|")
-        if (shipDate == queriedShipDate) shipDate.toList else List()
+        var shipDate = line.split("\\|")(10)
+        val dateFormatLength = queriedShipDate.split("\\-").length
+        // Check date format
+        if (dateFormatLength == 2) {
+          shipDate = shipDate.dropRight(3)
+        } else if (dateFormatLength == 1) {
+          shipDate = shipDate.dropRight(6)
+        }
+        if (shipDate == queriedShipDate) List(shipDate) else List()
       })
-      .map(word => (word, 1))
-      .reduceByKey(_ + _)
-      .saveAsTextFile(args.dateparam())
+    println("ANSWER=" + numberOfItems.count())
   }
 }
