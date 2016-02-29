b5c9cb25eb6a805a3ac7f7d554c141d191d1df41
Ehsanur Rahman <esnr.rahman@gmail.com>
2016-02-29 11:35:03 -0500

    start q2

---
+++ src/main/java/ca/uwaterloo/cs/bigdata2016w/esnrRahman/assignment5/Q2.scala
@@ -0,0 +1,49 @@
+package ca.uwaterloo.cs.bigdata2016w.esnrRahman.assignment5
+
+import org.apache.log4j._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.rogach.scallop._
+
+class Conf2(args: Seq[String]) extends ScallopConf(args) {
+  mainOptions = Seq(input, date)
+  val input = opt[String](descr = "input dir", required = true)
+  val date = opt[String](descr = "date param", required = true)
+}
+
+object Q2 {
+  val log = Logger.getLogger(getClass().getName())
+
+  def main(argv: Array[String]) {
+    val args = new Conf(argv)
+
+    log.info("Input: " + args.input())
+    log.info("Ship Date: " + args.date())
+
+    val conf = new SparkConf().setAppName("SQL Query 1")
+    val sc = new SparkContext(conf)
+    val queriedShipDate = args.date()
+
+    val lineItemtextFile = sc.textFile(args.input() + "/lineitem.tbl")
+    val orderTextFile = sc.textFile(args.input() + "/orders.tbl")
+
+    val numberOfItems = lineItemtextFile
+      .flatMap(line => {
+        val lineItemTable = line.split("\\|")
+        val orderKey = lineItemTable(0)
+        var shipDate = lineItemTable(10)
+        val dateFormatLength = queriedShipDate.split("\\-").length
+        // Check date format
+        if (dateFormatLength == 2) {
+          shipDate = shipDate.dropRight(3)
+        } else if (dateFormatLength == 1) {
+          shipDate = shipDate.dropRight(6)
+        }
+        if (shipDate == queriedShipDate) List((orderKey,shipDate)) else List()
+      })
+
+    for (i <- numberOfItems) {
+      println("(" + i._2 + "," + i._1 + ")")
+    }
+  }
+}
