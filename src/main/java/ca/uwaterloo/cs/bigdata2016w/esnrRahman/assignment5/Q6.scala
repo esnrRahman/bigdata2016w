aff972b447f51a86a35bee7a7413bfaecb62fe3f
Ehsanur Rahman <esnr.rahman@gmail.com>
2016-03-02 22:59:43 -0500

    WIP

--- src/main/java/ca/uwaterloo/cs/bigdata2016w/esnrRahman/assignment5/Q6.scala
+++ src/main/java/ca/uwaterloo/cs/bigdata2016w/esnrRahman/assignment5/Q6.scala
@@ -39,33 +39,42 @@ object Q6 {
         val ltLineStatus = ltOrderTable(9)
         val ltShipDate = ltOrderTable(10)
 
-        val sumDiscPrice = ltExtendedPrice.toDouble * (1.0d - ltDiscount.toDouble)
-        val sumCharge = ltExtendedPrice.toDouble * (1.0d - ltDiscount.toDouble) * (1.0d + ltTax.toDouble)
+        val discPrice = ltExtendedPrice.toDouble * (1.0d - ltDiscount.toDouble)
+        val charge = ltExtendedPrice.toDouble * (1.0d - ltDiscount.toDouble) * (1.0d + ltTax.toDouble)
 
-        (ltReturnFlag, ltLineStatus, ltQuantity, ltExtendedPrice, sumDiscPrice, sumCharge, 1.0d, ltDiscount.toDouble, ltShipDate)
+        (ltReturnFlag, ltLineStatus, ltQuantity, ltExtendedPrice, discPrice, charge, 1.0d, ltDiscount.toDouble, ltShipDate)
       })
       .filter(set => set._9.startsWith(queriedShipDate))
       .map(set => ((set._1, set._2), (set._3.toDouble, set._4.toDouble, set._5, set._6, set._7, set._8)))
-//          .reduceByKey((firstSet, secondSet) =>
-//            (firstSet._1 + secondSet._2,
-//              firstSet._2 + secondSet._2,
-//              firstSet._3, + secondSet._3,
-//              firstSet._4 + secondSet._4,
-//              firstSet._5, + secondSet._5,
-//              firstSet._6, + secondSet._6
-//              )
-//          ).collect().toList
+      .reduceByKey((firstSet, secondSet) => (
+        firstSet._1 + secondSet._1,
+        firstSet._2 + secondSet._2,
+        firstSet._3 + secondSet._3,
+        firstSet._4 + secondSet._4,
+        firstSet._5 + secondSet._5,
+        firstSet._6 + secondSet._6
+        )).collect().toList
+      .map(set => {
+        val sumQuantity = set._2._1
+        val sumBasePrice = set._2._2
+        val sumDiscPrice = set._2._3
+        val sumCharge = set._2._4
+        val returnFlag = set._1._1
+        val lineStatus = set._1._2
+        val total = set._2._5
+        val sumDiscount = set._2._6
+        (returnFlag, lineStatus, sumQuantity, sumBasePrice, sumDiscPrice, sumCharge, sumQuantity/total, sumBasePrice/total, sumDiscount/total)
+      })
 
     def show(x: Option[String]) = x match {
       case Some(s) => s
       case None => "?"
     }
 
-    val finalTable = lineItems.collect()
-    for (i <- finalTable) {
-      println("XXXXXXXXXX")
-      println("(" + i._1._1 + "," + i._1._2 + "," + i._2._1 + "," + i._2._2 + "," + i._2._3+ "," + i._2._4+ "," + i._2._5 + "," + i._2._6 + ")")
-      println("XXXXXXXXXX")
+    for (i <- lineItems) {
+      println("CHECK BELOW ---> ")
+      println("(" + i._1 + "," + i._2 + "," + i._3 + "," + i._4 + "," + i._5 + "," + i._6 + "," + i._7 + "," + i._8 + ")")
+      println()
     }
   }
 
