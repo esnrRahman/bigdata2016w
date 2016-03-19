a84b020113819963781ba0fc8fa061ca0071b5eb
Ehsanur Rahman <esnr.rahman@gmail.com>
2016-03-19 16:44:54 -0400

    WIP for ApplySpamClassifier

--- src/main/java/ca/uwaterloo/cs/bigdata2016w/esnrRahman/assignment6/ApplySpamClassifier.scala
+++ src/main/java/ca/uwaterloo/cs/bigdata2016w/esnrRahman/assignment6/ApplySpamClassifier.scala
@@ -8,15 +8,16 @@ import org.rogach.scallop.ScallopConf
 import scala.collection.mutable.Buffer
 
 class Conf2(args: Seq[String]) extends ScallopConf(args) {
-  mainOptions = Seq(output, model)
-  val output = opt[String](descr = "input file", required = true)
-  val model = opt[String](descr = "output dir", required = true)
+  mainOptions = Seq(input, output, model)
+  val input = opt[String](descr = "input file", required = true)
+  val output = opt[String](descr = "output dir", required = true)
+  val model = opt[String](descr = "model dir", required = true)
 }
 
 object ApplySpamClassifier {
   val log = Logger.getLogger(getClass().getName())
 
-  // w is the weight vector (make sure the variable is within scope)
+  // w is the weight vector
   var w = scala.collection.mutable.Map[Int, Double]()
 
   // Scores a document based on its list of features.
@@ -29,57 +30,70 @@ object ApplySpamClassifier {
   def main(argv: Array[String]) {
     val args = new Conf2(argv)
 
-    log.info("Input path" + args.output())
-    log.info("Output dir" + args.model())
+    log.info("Input path" + args.input())
+    log.info("Output dir" + args.output())
+    log.info("Model dir" + args.model())
 
-    val conf = new SparkConf().setAppName("Train Spam Classifier")
+    val conf = new SparkConf().setAppName("Apply Spam Classifier")
     val sc = new SparkContext(conf)
 
-    val outputDir = new Path(args.model())
+    val outputDir = new Path(args.output())
     FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
 
-    val textFile = sc.textFile(args.output())
+    val textFile = sc.textFile(args.input())
 
-    val trained = textFile.map(line => {
+    val modelFile = sc.textFile(args.model() + "/part-00000")
+
+    val modelMap = modelFile.map(line => {
+      val stringArray = line.split("\\,")
+      val feature = stringArray(0).drop(1).toInt
+      val weight = stringArray(1).dropRight(1).toDouble
+      w(feature) = weight
+    })
+
+    val result = textFile.map(line => {
       val trainingInstanceArray = line.split(" ")
       val docid = trainingInstanceArray(0)
       val label = trainingInstanceArray(1)
 
-      val features = trainingInstanceArray.toBuffer
-      features -= docid
-      features -= label
+      val featuresStringArray = trainingInstanceArray.toBuffer
 
-      (0, (docid, label, features))
-    })
-      .groupByKey(1)
-      .map(pair => {
-        // Then run the trainer...
+      featuresStringArray -= docid
+      featuresStringArray -= label
 
-        // This is the main learner:
-        val delta = 0.002
-
-        val pairList = pair._2.toList
-
-        pairList.foreach(tuple => {
-          // For each instance...
-          val docId = tuple._1
-
-          val label = tuple._2
-          // label
-          val featuresString = tuple._3 // feature vector of the training instance
-
-          val features = featuresString.map(_.toInt)
-
-          val score = spamminess(features)
-
-          if (score > 0)
-            (docId, label, score, "spam")
-          else
-            (docId, label, score, "ham")
-        })
+      var spamminessScore = 0d
+      featuresStringArray.foreach(feature => {
+        spamminessScore += w(feature.toInt)
       })
 
-    trained.saveAsTextFile(args.model())
+      if (spamminessScore > 0)
+        (docid, label, spamminessScore, "spam")
+      else
+        (docid, label, spamminessScore, "ham")
+
+//      (0, (docid, label, featuresStringArray))
+    })
+//      .groupByKey(1)
+//      .map(pair => {
+//
+//        val pairList = pair._2.toList
+//
+//        // each training instance
+//        pairList.foreach(tuple => {
+//
+//          // document ID
+//          val docId = tuple._1
+//          // label
+//          val label = tuple._2
+//
+//          val featuresString = tuple._3
+//          // features
+//          val features = featuresString.map(_.toInt)
+//
+//        })
+//      })
+
+    result.saveAsTextFile(args.output())
 
 
   }
